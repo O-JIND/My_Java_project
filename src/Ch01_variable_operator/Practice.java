5f883377a6b7f0b72db593740a5cda9f6c8abf18
JIND <akjlh00@gmail.com>
2025-07-23 14:52:30 +0900

    Fixed

---
+++ src/Ch01_variable_operator/Practice.java
@@ -0,0 +1,32 @@
+package Ch01_variable_operator;
+
+
+    public class Practice {
+        public static void main(String[] args) {
+        String name;
+        int age;
+        double height;
+        double weight;
+        String blood;
+        char alpha;
+        boolean bool;
+
+        name = "홍길동";
+        age = 30;
+        height=172.5 ;
+        weight= 78.8;
+        blood="AB";
+        alpha='F';
+        bool=false;
+
+        System.out.println("이름 : " + name);
+        System.out.println("나이 : " + age);
+        System.out.println("키 : "+ height);
+        System.out.println("몸무게 : "+ weight);
+        System.out.println("혈액형 : " + blood);
+        System.out.println("문자 : " + alpha);
+        System.out.println("진위 : " + bool);
+
+        }
+
+    }
