e4e5589d137c4dc83437e4774c8cf481536a07c1
HOME <bearfox1025@gmail.com>
2025-07-28 23:38:03 +0900

    done maybe

--- src/Practice_myself/Passwardck.java
+++ src/Practice_myself/Passwardck.java
@@ -2,43 +2,49 @@ package Practice_myself;
 import java.util.*;
 public class Passwardck {
     int i=0;
-    static class WeakPasswordException extends Exception{
+    boolean boo=false,numzon=false;
+    public static class WeakPasswordException extends Exception{
         public WeakPasswordException(String msg){
             super(msg);
         }
     }
 
-    public void length_ch(String k){
+    public  void length_ch(String k)throws WeakPasswordException{
         int K = k.length();
         if(K>7){
             for(i=0;i<K;i++){
                 char ch = k.charAt(i);
-                if((ch<90&&ch>65)||(ch>97&&ch<122)){
-                    boolean boo = true;
-                } else if (ch) {
-                    
+                if(Character.isLetter(ch)){
+                     boo = true;
+                } else {
+                    if(Character.isDigit(ch)) {
+                        numzon = true;
+                    }
                 }
 
+            }if(!(boo && numzon)) {
+                throw new WeakPasswordException("비밀번호 조건이 만족하지 않습니다.");
             }
+
+
         }else{
-            System.out.println("만족 x");
+            System.out.println("비밀번호는 8자리 이상이어야 합니다.");
         }
 
-
-
-
-
     }
-
-
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Passwardck ps= new Passwardck();
+    String a = sc.next();
 
-
-
-
-
+    try {
+        ps.length_ch(a);
+        if(ps.boo&&ps.numzon){
+            System.out.println("강력한 비밀번호입니다.");
+        }
+    }catch (WeakPasswordException e){
+        System.out.println("조건을 만족하지 않았습니다.");
     }
-
-
-
+        System.out.println("검사 완료");
+    }
 }
